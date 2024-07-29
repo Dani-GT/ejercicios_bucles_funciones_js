cf0739fa4087cc408e6ed88de57e6862e1624076
Dani-GT <dani_spain87@hotmail.com>
2024-07-29 19:27:25 +0200

    ejercicio 1 finalizado

---
+++ ejercicio10.js
@@ -0,0 +1,18 @@
+const capitals = {
+    Spain: 'Madrid',
+    France: 'Paris',
+    Italy: 'Rome',
+    Germany: 'Berlin',
+    Portugal: 'Lisbon',
+    Poland: 'Warsaw',
+    Greece: 'Athens',
+    Austria: 'Vienna',
+    Hungary: 'Budapest',
+    Ireland: 'Dublin'
+  };
+  
+  function getCapital(country) {
+    // insert code
+  }
+  
+  
